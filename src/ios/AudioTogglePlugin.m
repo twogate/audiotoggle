bbf70a2ee04f381c3a3ddb3068f21c30faa7962c
keiya <keiya_21@yahoo.co.jp>
2018-05-03 08:24:58 +0900

    MikelEiza/audiotoggle@eebf99ad4c82db82a2e7a4e3ca3c551219d2cc90

--- src/ios/AudioTogglePlugin.m
+++ src/ios/AudioTogglePlugin.m
@@ -6,21 +6,19 @@
 
 - (void)setAudioMode:(CDVInvokedUrlCommand *)command
 {
-    NSError* __autoreleasing err = nil;
-    NSString* mode = [NSString stringWithFormat:@"%@", [command.arguments objectAtIndex:0]];
-    
-    UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_None;
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    
-    if ([mode isEqualToString:@"earpiece"]) {
-        [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&err];
-        audioRouteOverride = kAudioSessionProperty_OverrideCategoryDefaultToSpeaker;
-        AudioSessionSetProperty(kAudioSessionProperty_OverrideAudioRoute, sizeof(audioRouteOverride), &audioRouteOverride);
-    } else if ([mode isEqualToString:@"speaker"] || [mode isEqualToString:@"ringtone"]) {
-        [session setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:&err];
-    } else if ([mode isEqualToString:@"normal"]) {
-        [session setCategory:AVAudioSessionCategorySoloAmbient error:&err];
-    }
+  NSError* __autoreleasing err = nil;
+  NSString* mode = [NSString stringWithFormat:@"%@", [command.arguments objectAtIndex:0]];
+
+  AVAudioSession *session = [AVAudioSession sharedInstance];
+
+  if ([mode isEqualToString:@"earpiece"]) {
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&err];
+    [session overrideOutputAudioPort:kAudioSessionProperty_OverrideAudioRoute error:&err];
+  } else if ([mode isEqualToString:@"speaker"]) {
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:&err];
+  } else if ([mode isEqualToString:@"normal"]) {
+    [session setCategory:AVAudioSessionCategorySoloAmbient error:&err];
+  }
 }
 
 @end
